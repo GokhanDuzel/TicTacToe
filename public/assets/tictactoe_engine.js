e8e14ad618a550eba9f8a26adef7ee0925f73522
Gokhan Duzel <43392060+GokhanDuzel@users.noreply.github.com>
2023-03-19 18:23:31 -0400

    tictactoe engine: initial board, create board

---
+++ public/assets/tictactoe_engine.js
@@ -0,0 +1,14 @@
+const gameboard = document.querySelector(".gameboard");
+const infos = document.querySelector(".infos");
+
+const initialBoard = [
+    "", "", "", 
+    "", "", "",
+    "", "", ""
+];
+
+function createGameBoard() {
+    initialBoard.forEach((cell, index) => {
+        const cell = document.createElement('div');
+    })
+}
